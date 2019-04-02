a6eef62ae5dc63f003e623e11cdc20fa7317f4f3
Daniel Stokes <kupomail@gmail.com>
2019-04-01 20:10:53 -0700

    Add initial buffer creation code

---
+++ panda3d_render_pass/renderpass.py
@@ -0,0 +1,51 @@
+import panda3d.core as p3d
+
+
+def create_default_framebuffer_properties():
+    props = p3d.FrameBufferProperties()
+    props.set_rgba_bits(8, 8, 8, 0)
+    props.set_depth_bits(24)
+    return props
+
+
+def create_window_properties(size_x, size_y):
+    return p3d.WindowProperties.size(size_x, size_y)
+
+def create_buffer(name, pipe, engine, window):
+    return engine.make_output(
+        pipe,
+        name,
+        0,
+        create_default_framebuffer_properties(),
+        p3d.WindowProperties(),
+        p3d.GraphicsPipe.BF_refuse_window | p3d.GraphicsPipe.BF_size_track_host,
+        window.get_gsg(),
+        window
+    )
+
+
+class RenderPass:
+    def __init__(
+            self,
+            name,
+            pipe=None,
+            engine=None,
+            window=None,
+    ):
+        self.name = name
+        self._pipe = pipe if pipe else base.pipe
+        self._engine = engine if engine else base.graphics_engine
+        self._window = window if window else base.win
+        self.output = p3d.Texture(f'{self.name}_output')
+        self.buffer = create_buffer(
+            self.name,
+            self._pipe,
+            self._engine,
+            self._window
+        )
+
+        self.buffer.add_render_texture(
+            self.output,
+            p3d.GraphicsOutput.RTM_bind_or_copy,
+            p3d.GraphicsOutput.RTP_color
+        )
--- tests/test_panda3d_render_pass.py
+++ tests/test_panda3d_render_pass.py
@@ -1,5 +1,43 @@
-from panda3d_render_pass import __version__
+import pytest
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+import panda3d.core as p3d
+
+from panda3d_render_pass import RenderPass
+
+def create_window(engine, pipe):
+    fbprops = p3d.FrameBufferProperties()
+    fbprops.set_rgba_bits(8, 8, 8, 0)
+    fbprops.set_depth_bits(24)
+    winprops = p3d.WindowProperties.size(1, 1)
+    flags = p3d.GraphicsPipe.BF_refuse_window
+    return engine.make_output(
+        pipe,
+        'window',
+        0,
+        fbprops,
+        winprops,
+        flags
+    )
+
+@pytest.fixture
+def default_args():
+    pipe = p3d.GraphicsPipeSelection.get_global_ptr().make_default_pipe()
+    engine = p3d.GraphicsEngine(pipe)
+    window = create_window(engine, pipe)
+    return {
+        'pipe': pipe,
+        'engine': engine,
+        'window': window
+    }
+
+
+def test_create_output(default_args):
+    engine = default_args['engine']
+    window = default_args['window']
+    initial_win_count = len(engine.get_windows())
+    initial_buffer_count = window.count_textures()
+    rpass = RenderPass('test', **default_args)
+    engine.render_frame()
+    assert len(engine.get_windows()) == initial_win_count + 1
+    assert type(rpass.buffer == p3d.GraphicsOutput)
